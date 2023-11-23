7fe631fd8e00c1699adb700ddcf206585b0d275b
Nithesh-16 <nitheshravagondu@gmail.com>
2023-11-23 18:20:40 +0530

    first commit

--- server.js
+++ server.js
@@ -1,43 +1,50 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
-const PORT = process.env.PORT || 5000;
+const dotenv = require("dotenv");
+const path = require("path");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 
-//connect to DB
+// Load environment variables
+const dotenvResult = dotenv.config();
+if (dotenvResult.error) {
+  console.error(dotenvResult.error);
+  process.exit(1); // Exit the process if there's an issue with dotenv
+}
 
+// Connect to the database
 connectDB();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Root route
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "welcome to Support Desk" });
+  res.status(200).json({ message: "Welcome to Support Desk" });
 });
-//CORES ISSUE
 
 // Routes
-
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
 
-// Serve Frontend
+// Serve Frontend in production
 if (process.env.NODE_ENV === "production") {
   // Set build folder as static
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  // FIX: below code fixes app crashing on refresh in deployment
+  // Handle client-side routing by serving index.html
   app.get("*", (_, res) => {
     res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
   });
-} else {
-  app.get("/", (req, res) => {
-    res.status(200).json({ message: "Welcome to the Support Desk API" });
-  });
 }
 
+// Error handling middleware
 app.use(errorHandler);
 
+// Start the server
 app.listen(PORT, () => {
-  console.log(`server started on Port ${PORT}`);
+  console.log(`Server started on Port ${PORT}`);
 });
